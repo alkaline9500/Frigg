0b803e48edf3d7f2215460873b34ecf2e37ceb15
Nic Manoogian <zimmoz3@verizon.net>
2015-12-23 11:29:47 -0500

    Changes UI control to a color square.

--- Frigg/ViewController.swift
+++ Frigg/ViewController.swift
@@ -13,28 +13,73 @@ class ViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var authKeyButton: UIButton!
     
+    @IBOutlet weak var garageButton: UIButton!
     let manager = ValhallaAPIManager()
+    
+    var colorTimer: NSTimer? = nil
+    var holdTime = 0.0
+    var currentBlue = 0.0 {
+        didSet {
+            garageButton.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: CGFloat(currentBlue), alpha: 1.0)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resetButton()
+        resetTimer()
+        garageButton.addTarget(self, action: "didDownButton:", forControlEvents: UIControlEvents.TouchDown)
+        garageButton.addTarget(self, action: "didReleaseButton:", forControlEvents: UIControlEvents.TouchUpInside)
+        garageButton.addTarget(self, action: "didReleaseButton:", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func didToggleGarage(sender: UIButton) {
-        manager.toggleGarage { response in
-            if response.success {
-                self.statusLabel.textColor = UIColor.blackColor()
-            }
-            else {
-                self.statusLabel.textColor = UIColor.redColor()
-            }
-            self.statusLabel.text = response.text
+    
+    // MARK: Button Actions
+    
+    func didDownButton(sender: UIButton) {
+        if colorTimer == nil {
+            colorTimer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "incrementColor", userInfo: nil, repeats: true)
         }
+    }
+
+    func didReleaseButton(sender: UIButton) {
+        resetButton()
+        resetTimer()
+    }
+    
+    func incrementColor() {
+        if holdTime <= 0.0 {
+            self.resetTimer()
+            garageButton.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+            manager.toggleGarage { response in
+                if response.success {
+                    self.statusLabel.textColor = UIColor.blackColor()
+                }
+                else {
+                    self.statusLabel.textColor = UIColor.redColor()
+                }
+                self.statusLabel.text = response.text
+                self.resetButton()
+            }
+            return
+        }
+        currentBlue += 0.05
+        holdTime -= 0.1
+    }
+    
+    private func resetButton() {
+        currentBlue = 0.0
+        garageButton.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    }
+    
+    private func resetTimer() {
+        colorTimer?.invalidate()
+        colorTimer = nil
+        holdTime = 3.0
     }
 
     @IBAction func setAuthKey(sender: UIButton) {
